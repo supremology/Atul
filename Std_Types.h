57b8416b5822601d9b486620381a2857f94c9846
Atul Zalke <atulzalke@yahoo.com>
2019-06-11 13:12:37 +0530

    Update Std_Types.h

--- Std_Types.h
+++ Std_Types.h
@@ -18,22 +18,22 @@ typedef unsigned char StatusType;
 /**** Symbol Definitions ****/
 #ifndef STATUSTYPEDEFINED
 #define STATUSTYPEDEFINED
-#define E_OK 0x00u
+#define E_OK 		0x00u
 #endif
 
-#define E_NOT_OK 0x01u
+#define E_NOT_OK 	0x01u
 
 /* Physical state 5V or 3.3V */
-#define STD_HIGH 0x01u
+#define STD_HIGH 	0x01u
 /* Physical state 0V */
-#define STD_LOW 0x00
+#define STD_LOW 	0x00
 /* Logical state active */
-#define STD_ACTIVE 0x01u
+#define STD_ACTIVE	0x01u
 /* Logical state idle */
-#define STD_IDLE 0x00u
+#define STD_IDLE 	0x00u
 
-#define STD_ON 0x01u
-#define STD_OFF 0x00u
+#define STD_ON 		0x01u
+#define STD_OFF 	0x00u
 
 /**** Type Definition ****/
 struct Std_VersionInfoType {
